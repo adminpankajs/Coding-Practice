78791f5334d2087be874a76a2512015c0ad398b7
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-03-03 15:08:33 +0530

    Update 4_max_one_row.cpp

--- Competitive/MATRIX/4_max_one_row.cpp
+++ Competitive/MATRIX/4_max_one_row.cpp
@@ -2,19 +2,31 @@
 using namespace std;
     
 int main(){
+    int len  = 4;
+    int pos = len-1;
+    int max;
     int arr[len][len] =
-    {{0, 1, 1, 1},
+    {{0, 0, 0, 1},
     {0, 0, 1, 1},
     {1, 1, 1, 1},
-    {0, 0, 0, 0}};
+    {0, 1, 1, 1}};
     for (int i = 0; i < len; i++)
     {
-        for (int j = 0; j < len; j++)
+        for (int j = pos; j >= 0; j--)
         {
-            if()
+            if(arr[i][j]==0)
+            {
+                pos=j;
+                cout<<pos<<endl;
+                break;
+            }
+            if(j==0 && arr[i][j]==1)
+            {
+                pos=-1;
+                break;
+            }
         }
-        
     }
-    
+    cout<<"MAX 1's : "<<(len-pos-1);
     return 0;
 }
