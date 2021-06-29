c220ed11ef02634581b5205a554b920f9c2743eb
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-29 17:12:42 +0530

    list completed.

--- Competitive/CPP STL/list_STL.cpp
+++ Competitive/CPP STL/list_STL.cpp
@@ -28,9 +28,20 @@ printRList(list<int> myList)
     cout<<endl;
 }
 
+bool even(const int& value)
+{
+    if(value%2==0)
+        return true;
+    return false;
+}
+
 int main()
 {
     list<int> myList = {1,2,3,4,5,6};
+    list<int> l1 = { 1, 2, 3 };
+    list<int> l2 = { 4, 5 };
+    list<int> l3 = { 6, 7, 8 };
+  
     auto itr = myList.begin();
     // printList(myList);
     // myList.push_front(10);
@@ -46,8 +57,28 @@ int main()
     myList.insert(itr,1,30);
     printList(myList);
     itr = myList.begin();
-    myList.erase();
+    advance(itr,1);
+    myList.erase(itr);
     printList(myList);
-    
+    // myList.assign(2,5);
+    // printList(myList);
+    // myList.remove(3);
+    myList.remove_if(even);
+    printList(myList);
+    myList.reverse();
+    printList(myList);
+    cout<<myList.size();
+    myList.resize(4,6);
+    printList(myList);
+    myList.sort();
+    myList.reverse();
+    printList(myList);
+    cout<<myList.max_size();
+    myList.clear();
+    printList(myList);
+    itr = l1.begin();
+    advance(itr,1);
+    l1.splice(itr, l2);
+    printList(l1);
     return 0;
 }
