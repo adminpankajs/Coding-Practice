504bf43e9931403db6676ccec10c2cf80c205864
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-29 16:45:54 +0530

    add : list_STL

---
+++ Competitive/CPP STL/list_STL.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+
+using namespace std;
+
+printList(list<int> myList)
+{
+    auto itr = myList.begin();
+    cout<<endl;
+    while (itr != myList.end())
+    {
+        cout<<*itr<<" ";
+        itr++;
+    }
+    cout<<endl;
+}
+
+printRList(list<int> myList)
+{
+    auto itr = myList.rbegin();
+    cout<<endl;
+    while (itr != myList.rend())
+    {
+        cout<<*itr<<" ";
+        itr++;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    list<int> myList = {1,2,3,4,5,6};
+    auto itr = myList.begin();
+    // printList(myList);
+    // myList.push_front(10);
+    // myList.push_back(20);
+    // printList(myList);
+    // myList.pop_front();
+    // printList(myList);
+    // myList.pop_back();
+    // printList(myList);
+    // printRList(myList);
+    // cout<<myList.empty();
+    advance(itr,1);
+    myList.insert(itr,1,30);
+    printList(myList);
+    itr = myList.begin();
+    myList.erase();
+    printList(myList);
+    return 0;
+}
