9173760238aa1f8547155475403f958388416163
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-07-01 12:36:34 +0530

    Rotated_String

--- Competitive/Daily Practice/test.cpp
+++ Competitive/Daily Practice/test.cpp
@@ -5,31 +5,48 @@
 
 using namespace std;
 
+ifstream infile("input.txt");
+ofstream outfile("output.txt", ios::trunc);
+
+void solve(string input, string length, string dir)
+{
+    int count = 0;
+    string str1, str2, str3;
+    if(dir=="L")
+    {
+        str1 = input.substr(0,stoi(length));
+        str2 = input.substr(stoi(length));
+        str3 = str2.append(str1);
+    }
+    else
+    {
+        str1 = input.substr(0,input.size()-stoi(length));
+        str2 = input.substr(input.size()-stoi(length));
+        str3 = str2.append(str1);
+    }
+    outfile<<str3;
+}
+
 int main()
 {
-    ifstream infile("input.txt");
-    ofstream outfile("output.txt", ios::trunc);
     if(infile.fail() || outfile.fail())
     {
         cout<<"No such file found !!"<<endl;
     }
     else
     {
-        string s;
-        while (infile>>s)
+        string input;
+        while (infile>>input)
         {
-            string str;
-            int num = stoi(s);
-            if(num%2==0)
-            {
-                num = (num-2)/2;
-            }
-            else
-            {
-                num = num-1;
-            }
-            outfile<<num<<endl;
+            string str,length,dir;
+            str = input.substr(0,input.find(','));
+            input = input.substr(input.find(',')+1);
+            length = input.substr(0,input.find(','));
+            input = input.substr(input.find(',')+1);
+            dir = input.substr(0,input.find(','));
+            solve(str, length, dir);
         }
+        cout<<"Success !!"<<endl;
     }
     return 0;
 }
